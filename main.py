48933702879042bc9cbcb25e928627ad6545c36f
Prakash Chaudhary <connectwithprakash@yahoo.com>
2019-02-12 23:51:28 +0545

    First commit
    Contains trained model and main.py file to run live emotion recognition

---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Live Sentiment Analysis
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+from keras.models import load_model
+
+
+# In[2]:
+
+
+#handler for camera
+camera_handle = cv2.VideoCapture(0)
+
+#loading pre trained haar code for face detection
+face_haar = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#loading saved cnn model
+model = load_model('live_emotion_recognition_model.h5')
+
+#predictiong face emotion using saved model
+def get_emotion(image, shape_x, shape_y):
+    #reshape to the size of face images of model trained on
+    image = cv2.resize(image,(shape_x, shape_y))
+    image = image.reshape((1, shape_x, shape_y, 1))
+    result = model.predict_classes(image, verbose=0)
+    emotions = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
+    return emotions[result[0]]
+
+
+# In[3]:
+
+
+#main loop
+while True:
+    _, rgb_image = camera_handle.read()
+    flipped_rgb_image = cv2.flip(rgb_image, 1)
+    gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+    faces = face_haar.detectMultiScale(gray_image, 1.3, 5)#to tune
+    
+    for (x, y, w, h) in faces:
+        gray_face = gray_image[y:y+h, x:x+w]
+        #specify model face size here 48x48
+        emotion = get_emotion(gray_face, 48, 48)
+        cv2.rectangle(flipped_rgb_image,(x,y),(x+w,y+h),(0, 255, 0),1)
+        cv2.putText(flipped_rgb_image, emotion, (30,30), font, 0.8, (0, 255, 0), 1)
+    
+    cv2.imshow('rgb', flipped_rgb_image)
+    
+    k = cv2.waitKey(1) & 0xEFFFFF
+    if k==27:   
+        break
+    else:
+        continue
+        
+cv2.destroyAllWindows()
+
